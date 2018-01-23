d6723673c1c77ee150b29db318f51d9238d6f37b
Leocodefocus <837567340@qq.com>
2018-01-23 17:06:46 +0800

    add data.txt

--- bkpro/genRandomData.cpp
+++ bkpro/genRandomData.cpp
@@ -10,7 +10,7 @@ const unsigned long size = 10000000;
 int num[size];
 
 unsigned long randint(unsigned long l,unsigned long u){
-    printf("%d\n",RAND_MAX);
+    //printf("%d\n",RAND_MAX);
     return l+(rand()*RAND_MAX+rand())%(u-l+1);  
 }
 
@@ -21,14 +21,16 @@ int main(int argc,char* argv[]){
     srand((unsigned)time(NULL));
     unsigned long i;
     for(n=0;n<size;n++){
-        printf("%ld\n",n);
+        //printf("%ld\n",n);
         i = randint(n,size-1);
-        printf("%ld\n",i);
+        //printf("%ld\n",i);
         printf("%ld %d %ld %d\n",i,num[i],n,num[n]);
         int nm = num[n];num[n]=num[i];num[i]=nm;
     }
-    for(n=0;n<size;n++)        
+    for(n=0;n<size;n++){        
         fprintf(fp,"%d ",num[n]);
+        if((n+1)%1000==0) fprintf(fp,"%s","\n");
+    }
     fclose(fp);
     return 0;
 }
